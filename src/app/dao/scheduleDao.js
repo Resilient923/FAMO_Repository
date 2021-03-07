92d07675898b84165c3f38d1f4ff789b8bc946bb
Resilient923 <kc960923@naver.com>
2021-03-07 11:30:19 +0900

    [update] schedule 210307

--- src/app/dao/scheduleDao.js
+++ src/app/dao/scheduleDao.js
@@ -20,6 +20,71 @@ async function insertscheduleInfo(insertscheduleParams) {
   return  insertschedulerows;
 }
 
+//일정 수정
+async function updatescheduleInfo(updatescheduleParams) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const updatescheduleQuery = `
+        
+  update schedule
+  set scheduleName=?,
+  scheduleDate=?,
+  scheduleCategoryID=?,
+  scheduleMemo=?,
+  scheduleUpdatedAt=current_timestamp
+  where scheduleID = ?;
+    `;
+  
+  const updatescheduleRow = await connection.query(
+    updatescheduleQuery, 
+    updatescheduleParams 
+  );
+  connection.release();
+  return updatescheduleRow;
+}
+//일정 조회
+async function getscheduleInfo(updatescheduleParams) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const updatescheduleQuery = `
+        
+  select 
+  userID,
+  date_format(scheduleDate,'%e %b') as 'scheduleDate',
+       scheduleName,
+       scheduleMemo,
+       schedulePick
+    from schedule
+    
+    `;
+  
+  const updatescheduleRow = await connection.query(
+    updatescheduleQuery, 
+    updatescheduleParams 
+  );
+  connection.release();
+  return updatescheduleRow;
+}
+//일정삭제
+async function deletescheduleInfo(scheduleID) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const deletescheduleQuery = `
+        
+  update schedule
+  set scheduleDelete = -1
+  where scheduleID='${scheduleID}';
+
+    
+    `;
+  
+  const deletescheduleRow = await connection.query(
+    deletescheduleQuery, 
+   
+  );
+  connection.release();
+  return deletescheduleRow;
+}
 module.exports = {
-  insertscheduleInfo
+  insertscheduleInfo,
+  updatescheduleInfo,
+  getscheduleInfo,
+  deletescheduleInfo
 };
--- src/app/routes/scheduleRoute.js
+++ src/app/routes/scheduleRoute.js
@@ -3,5 +3,8 @@ module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
     /* 연결 테스트 */
-    app.post('/schedule',schedule.insertschedule);
+    app.post('/schedules',schedule.insertschedule); // 일정생성
+    app.patch('/schedules/:scheduleID',schedule.updateschedule); //일정수정
+    app.get('/schedules',schedule.getschedule); //일정조회
+    app.put('/schedules/:scheduleID',schedule.deleteschedule); //일정삭제
 };
