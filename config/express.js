2e0cd176f1ce22fdd6b81fa8ca339ca0323a5b79
Resilient923 <kc960923@naver.com>
2021-03-07 11:38:27 +0900

    [update] express 210307

--- config/express.js
+++ config/express.js
@@ -19,6 +19,14 @@ module.exports = function () {
     /* App (Android, iOS) */
     require('../src/app/routes/indexRoute')(app);
     require('../src/app/routes/userRoute')(app);
+    
+    
+    
+    
+    
+    
+    
+    
     require('../src/app/routes/scheduleRoute')(app);
 
     /* Web */
