de80cb6dbe646f64c97acb87ccf0469509cb2de7
sojungoh <sojung3514@gmail.com>
2021-03-12 23:55:26 +0900

    [soi] merge to main

--- src/app/controllers/scheduleController.js
+++ src/app/controllers/scheduleController.js
@@ -3,17 +3,18 @@ const {logger} = require('../../../config/winston');
 const scheduleDao = require('../dao/scheduleDao');
 //오늘일정생성
 exports.inserttodayschedule = async function (req, res) {
-   const {
+     const {
         scheduleName,scheduleTime,scheduleCategoryID,scheduleMemo
-    } = req.body;
+     } = req.body;
 
-    if (!scheduleName) {
-     res.json({
+     if (!scheduleName) {
+        res.json({
             isSuccess: false,
             code: 206,
             message: "일정제목을 입력해주세요."
         });
-    }
+     }
+
     if (scheduleName.length >=50) {
      res.json({
             isSuccess: false,
@@ -44,13 +45,13 @@ exports.inserttodayschedule = async function (req, res) {
 
         });
         connection.release();
-        } catch (err) {
-            // await connection.rollback(); // ROLLBACK
-            // connection.release();
-            logger.error(`오늘일정생성 에러\n: ${err.message}`);
+     } catch (err) {
+        // await connection.rollback(); // ROLLBACK
+        connection.release();
+        logger.error(`오늘일정생성 에러\n: ${err.message}`);
          res.status(401).send(`Error: ${err.message}`);
-        }
-};
+     }
+    };
 //일정생성(월캘린더에서 생성)
 exports.insertschedule = async function (req, res) {
     const {
@@ -118,7 +119,7 @@ exports.insertschedule = async function (req, res) {
          connection.release();
          } catch (err) {
              // await connection.rollback(); // ROLLBACK
-             // connection.release();
+             connection.release();
              logger.error(`일정생성 에러\n: ${err.message}`);
              res.status(401).send(`Error: ${err.message}`);
          }
@@ -186,9 +187,9 @@ exports.updateschedule = async function (req, res) {
         }
         connection.release();
     } catch (err) {
-
+        connection.release();   
         logger.error(`일정수정에러\n ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
+        res.status(401).send(`Error: ${err.message}`);
     }
 };
 //일정 조회
@@ -200,35 +201,29 @@ exports.getschedule = async function (req, res) {
             const getschedulerows = await scheduleDao.getscheduleInfo(userID);
 
             if (getschedulerows) {
-
-
-        const getschedulerows = await scheduleDao.getscheduleInfo(userID);
-
-        if (getschedulerows) {
-
-         res.json({
-                isSuccess: true,
-                code: 100,
-                message: userID + "번 유저 일정 조회 성공",
-                data : getschedulerows[0]
-                
-
-            });
-
-        }else{
-         res.json({
-                isSuccess: false,
-                code: 307,
-                message: "일정 조회 실패"
-            });
+                res.json({
+                       isSuccess: true,
+                       code: 100,
+                       message: userID + "번 유저 일정 조회 성공",
+                       data : getschedulerows[0]
+                   });
+               }else{
+                res.json({
+                       isSuccess: false,
+                       code: 307,
+                       message: "일정 조회 실패"
+                   });
+               }
+               connection.release();
+            }catch (err) {
+                connection.release();
+                logger.error(`일정 조회\n ${err.message}`);
+                res.status(401).send(`Error: ${err.message}`);
+            }
+        }catch (err) {
+            logger.error(`일정 조회\n ${err.message}`);
+            res.status(401).send(`Error: ${err.message}`);
         }
-        connection.release();
-    } catch (err) {
-
-
-        logger.error(`일정 조회\n ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
-    }
 };
 //카테고리별일정조회
 exports.getschedulebycategory = async function (req, res) {
@@ -259,9 +254,9 @@ exports.getschedulebycategory = async function (req, res) {
         }
         connection.release();
     } catch (err) {
-
+        connection.release();
         logger.error(`카테고리별 일정 조회\n ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
+        res.status(401).send(`Error: ${err.message}`);
     }
 };
 //일정 삭제
@@ -304,9 +299,9 @@ exports.deleteschedule = async function (req, res) {
         }
         connection.release();
     } catch (err) {
-
+        connection.release();
         logger.error(`일정 삭제 error: ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
+        res.status(401).send(`Error: ${err.message}`);
     }
 };
 //일정 즐겨찾기/즐겨찾기 취소
@@ -317,16 +312,13 @@ exports.patchschedulepick = async function (req, res) {
         const connection = await pool.getConnection(async (conn) => conn);
 
         const patchschedulepickrows = await scheduleDao.patchschedulepickInfo(scheduleID,userID);
-        console.log(patchschedulepickrows);
+        
         if (patchschedulepickrows) {
 
          res.json({
                 isSuccess: true,
                 code: 100,
                 message: userID + "번 유저 "+ scheduleID +"번 일정 즐겨찾기수정"
-                
-                
-
             });
 
         }else{
@@ -338,9 +330,9 @@ exports.patchschedulepick = async function (req, res) {
         }
         connection.release();
     } catch (err) {
-
+        connection.release();
         logger.error(`일정 즐겨찾기수정 조회\n ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
+        res.status(401).send(`Error: ${err.message}`);
     }
 };
 //일정 완료 버튼
@@ -358,9 +350,6 @@ exports.updateachievementschedule = async function (req, res) {
                 isSuccess: true,
                 code: 100,
                 message: userID + "번 유저 "+ scheduleID +"번 일정 완료/미완료"
-                
-                
-
             });
 
         }else{
@@ -372,9 +361,9 @@ exports.updateachievementschedule = async function (req, res) {
         }
         connection.release();
     } catch (err) {
-        
+        connection.release();
         logger.error(`일정 완료수정 조회\n ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
+        res.status(401).send(`Error: ${err.message}`);
     }
 };
 //유저별 총 해낸 일정수 조회
@@ -392,8 +381,6 @@ exports.getdoneschedulecount = async function (req, res) {
                 code: 100,
                 message: userID + "번 유저 해낸 일정 개수 조회 성공",
                 data : getdoneschedulecountrows[0]
-                
-
             });
 
         }else{
@@ -405,8 +392,8 @@ exports.getdoneschedulecount = async function (req, res) {
         }
         connection.release();
     } catch (err) {
-        
+        connection.release();
         logger.error(`해낸 일정 개수 조회\n ${err.message}`);
-     res.status(401).send(`Error: ${err.message}`);
+        res.status(401).send(`Error: ${err.message}`);
     }
 };
