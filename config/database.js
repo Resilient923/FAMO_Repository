87187126e0d59eb8feb81a32fa024e7394339d74
sojungoh <sojung3514@gmail.com>
2021-03-28 17:11:27 +0900

    db 변경

--- config/database.js
+++ config/database.js
@@ -3,24 +3,24 @@ const databaseInfo = require('./databasepwd');
 const {logger} = require('./winston');
 
 /*rds prod DB*/
-const pool = mysql.createPool({
-    host: 'rp.c4wybh857q9a.ap-northeast-2.rds.amazonaws.com',
-    user: 'steady',
-    port: 3306,
-    password: databaseInfo.rdspwd,
-    database: 'Real_famo',
-    dateStrings: 'date'
-});
-
-/*rds dev DB*/
 // const pool = mysql.createPool({
 //     host: 'rp.c4wybh857q9a.ap-northeast-2.rds.amazonaws.com',
 //     user: 'steady',
 //     port: 3306,
 //     password: databaseInfo.rdspwd,
-//     database: 'famodb',
+//     database: 'Real_famo',
 //     dateStrings: 'date'
 // });
+
+/*rds dev DB*/
+const pool = mysql.createPool({
+    host: 'rp.c4wybh857q9a.ap-northeast-2.rds.amazonaws.com',
+    user: 'steady',
+    port: 3306,
+    password: databaseInfo.rdspwd,
+    database: 'famodb',
+    dateStrings: 'date'
+});
 
 /*soi local test DB*/
 // const pool = mysql.createPool({
