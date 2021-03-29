7bee107c981026e864ae122daa07da99221f3289
Resilient923 <64014651+Resilient923@users.noreply.github.com>
2021-03-29 16:26:07 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -32,17 +32,6 @@ const {logger} = require('./winston');
 //     dateStrings: 'date'
 // });
 
-
-/*rds dev DB*/
-const pool = mysql.createPool({
-    host: 'rp.c4wybh857q9a.ap-northeast-2.rds.amazonaws.com',
-    user: 'steady',
-    port: 3306,
-    password: databaseInfo.rdspwd,
-    database: 'famodb',
-    dateStrings: 'date'
-});
-
 /*soi local test DB*/
 // const pool = mysql.createPool({
 //     host: '127.0.0.1',
@@ -52,16 +41,6 @@ const pool = mysql.createPool({
 //     database: 'famodb',
 //     dateStrings: 'date'
 // });
-
-/* steady test DB */
-/*  const pool = mysql.createPool({
-     host: 'rp.c4wybh857q9a.ap-northeast-2.rds.amazonaws.com',
-      user: 'steady',
-      port: 3306,
-      password: databaseInfo.rdspwd,
-      database: 'famo_steady',
-      dateStrings: 'date'
-  }); */
 
 module.exports = {
     pool: pool
